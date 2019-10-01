b394bd70f055a413a39c5d0080f3a4c123439f2d
Xiaoming Fu <xmingfu86@gmail.com>
2019-09-30 21:44:07 -0700

    roll method

--- src/RollDice.js
+++ src/RollDice.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Die from './Die';
+import './RollDice.css'
 
 class RollDice extends Component {
     static defaultProps = {
@@ -10,14 +11,30 @@ class RollDice extends Component {
             { num: 'four' },
             { num: 'five' },
             { num: 'six' },
-        ]}
-render(){
-    let randNum = Math.floor(Math.random()*6);
-    let randDie = this.props.diceNum[randNum];
-    return (
-        <Die rando={randDie.num}/>
-    )
-}
+        ]
+    }
+    constructor(props) {
+        super(props);
+        this.state = { randNum: 'one' };
+        this.roll = this.roll.bind(this);
+    }
+    roll(e) {
+        let randNum = this.props.diceNum[Math.floor(Math.random() * 6)].num;
+        this.setState({randNum :randNum});
+    }
+    render() {
+        
+        return (
+            <div className="RollDice">
+                <div className="RollDice-die">
+                    <Die rando={this.state.randNum} />
+                    <Die rando={this.state.randNum} />
+                </div>
+                <button className="btn btn-default btn-lg" onClick={this.roll}>Roll Dice!</button>
+            </div>
+
+        )
+    }
 }
 
 export default RollDice;
